71aa935f868fcd9c9e302e90c3b7f25372a42e2e
Jeff7531 <jeffmkwang@gmail.com>
2022-08-01 01:03:45 -0700

    trying to fix http crash

--- webapp.py
+++ webapp.py
@@ -5,13 +5,12 @@ from flask import Flask, request, jsonify, render_template, redirect, session
 
 app = Flask(__name__)
 
-@app.route("/", methods=["GET", "POST"])
+@app.route("/")
 def login():
     return render_template("home.html"), 200
 
 
 if __name__ == "__main__":
-    
     app.run(debug=True)
 
 
